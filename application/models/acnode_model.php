9a59a4ff2fc01cd5a3523918b03e0d939c2f8943
Oskar Pearson <oskar@deckle.co.uk>
2013-05-01 20:07:30 +0100

    Initial version

---
+++ application/models/acnode_model.php
@@ -0,0 +1,23 @@
+<?php
+
+	class acnode_model extends CI_Model
+	{
+		public function __construct()
+		{
+			$this->load->database();
+		}
+		
+		public function get_status($node_id){
+			$query = $this->db->get_where('acnodes',array('acnode_id' => $node_id));
+            $row = $query->row_array();
+            
+            $tool_id = $row["tool_id"];
+
+            $query = $this->db->get_where('tools',array('tool_id' => $tool_id));
+            $row = $query->row_array();
+
+            return (int) $row["status"];
+			
+		}
+	}
+?>
